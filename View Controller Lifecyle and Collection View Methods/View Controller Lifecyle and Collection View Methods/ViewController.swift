1806f7c915c822d192a2c4821a8d6474b8322717
brennadev <36449761+brennadev@users.noreply.github.com>
2018-02-16 11:04:43 -0600

    Update ViewController.swift

--- View Controller Lifecyle and Collection View Methods/View Controller Lifecyle and Collection View Methods/ViewController.swift
+++ View Controller Lifecyle and Collection View Methods/View Controller Lifecyle and Collection View Methods/ViewController.swift
@@ -3,7 +3,7 @@
 //  Collection view test
 //
 //  Created by Brenna Olson on 5/14/16.
-//  Copyright © 2016-2017 Brenna Olson. All rights reserved.
+//  Copyright © 2016-2018 Brenna Olson. All rights reserved.
 //
 //  REUSE AND REDISTRIBUTION NOTICE: You are free to download, use, and modify this code for personal use, provided that this copyright disclaimer appears in all files included in the original work. You may not redistribute this code in any form including, but not limited to, source or binary.
 
